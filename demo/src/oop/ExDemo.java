9b1ccf49dacc717b688ba06ae73ab6fb5c22d596
srikanthpragada <srikanthpragada@gmail.com>
2020-07-23 20:56:55 +0530

    23-JULY

---
+++ demo/src/oop/ExDemo.java
@@ -0,0 +1,16 @@
+package oop;
+
+public class ExDemo {
+
+	public static void main(String[] args)  {
+
+		int n = Integer.parseInt("abc");
+		System.out.println(n);
+		
+		// fun();
+	}
+	
+	public static void fun() throws InterruptedException  {
+		Thread.sleep(1000);
+	}
+}
